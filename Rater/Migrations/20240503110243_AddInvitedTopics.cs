efc5eb761144d173d6b197acc60d0c7d26d3fb33
Madrid-Born <alirezanoori.1381@gmail.com>
2024-05-03 15:08:52 +0330

    add migration

---
+++ Rater/Migrations/20240503110243_AddInvitedTopics.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rater.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvitedTopics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvitedTopicsIdJson",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvitedTopicsIdJson",
+                table: "Users");
+        }
+    }
+}
